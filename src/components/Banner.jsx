bdfabcea04ababded67eef32c6130d8f6ea2bd24
midu100 <kazimridulwebdev@gmail.com>
2025-07-07 01:07:33 +0600

    Banner part

---
+++ src/components/Banner.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import bannerBg from "../assets/img/bannerBg.png";
+import { Link } from "react-router";
+import { CiSearch } from "react-icons/ci";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const Banner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <>
+      <div className="slider-container">
+        <Slider {...settings}>
+          <div>
+            <section
+              id="banner"
+              style={{
+                background: `url(${bannerBg})`,
+                backgroundRepeat: "no-repeat",
+                // backgroundPosition: "center",
+                backgroundSize: "cover",
+              }}
+              className="lg:py-[148px] pt-[163px] pb-[199px] bg-left lg:bg-center"
+            >
+              <div className="container">
+                <div className="bannerText">
+                  <h2 className="text-[20px] font-medium font-poppins text-primary">
+                    Starting from: $49.99
+                  </h2>
+                  <h1 className="text-[30px] lg:text-[64px] font-semibold font-poppins text-second w-full lg:w-[632px] leading-[38px] lg:leading-[72px] mb-[40px] lg:my-[24px]">
+                    Exclusive collection for everyone
+                  </h1>
+                  <Link
+                    className="flex gap-[10px] items-center py-[12px] px-[24px] lg:py-[20px] lg:px-[36px] bg-second rounded-full w-fit text-[16px] font-poppins font-medium text-white lg:hover:scale-[1.1] duration-[.4s]"
+                    to={"#"}
+                  >
+                    Explore now{" "}
+                    <CiSearch className="text-[20px] text-[#F8FAFC]" />
+                  </Link>
+                </div>
+              </div>
+            </section>
+          </div>
+          <div>
+            <section
+              id="banner"
+              style={{
+                background: `url(${bannerBg})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+              }}
+              className="lg:py-[148px] pt-[163px] pb-[199px]"
+            >
+              <div className="container">
+                <div className="bannerText">
+                  <h2 className="text-[20px] font-medium font-poppins text-primary">
+                    Starting from: $49.99
+                  </h2>
+                  <h1 className="text-[30px] lg:text-[64px] font-semibold font-poppins text-second w-full lg:w-[632px] leading-[38px] lg:leading-[72px] mb-[40px] lg:my-[24px]">
+                    Exclusive collection for everyone
+                  </h1>
+                  <Link
+                    className="flex gap-[10px] items-center py-[12px] px-[24px] lg:py-[20px] lg:px-[36px] bg-second rounded-full w-fit text-[16px] font-poppins font-medium text-white"
+                    to={"#"}
+                  >
+                    Explore now{" "}
+                    <CiSearch className="text-[20px] text-[#F8FAFC]" />
+                  </Link>
+                </div>
+              </div>
+            </section>
+          </div>
+        </Slider>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
--- src/components/ResNav.jsx
+++ src/components/ResNav.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 import resLogo from "../assets/img/ResLogo.png";
 import { Link } from "react-router";
-import { RiSearch2Line } from "react-icons/ri";
+import { RiSearch2Line, RiUserLine } from "react-icons/ri";
+import { PiShoppingCartSimple } from "react-icons/pi";
 
 const ResNav = () => {
-    // hook or useState
-    const [showNav , setShowNav] = useState(false)
-
+  // hook or useState
+  const [showNav, setShowNav] = useState(false);
 
   return (
     <>
       <nav id="resNav" className="lg:hidden px-[16px] py-[20px]">
         <div className="container">
           <div className="resRow flex justify-between items-center">
-
             <div className="navLogo">
-              <Link to={'/'}>
+              <Link to={"/"}>
                 <img src={resLogo} alt="logo" />
               </Link>
             </div>
@@ -29,22 +28,53 @@ const ResNav = () => {
               />
             </div>
 
-            <button onClick={()=>setShowNav(!showNav)} className="w-[30px] h-[30px] relative">
-                <span className={`w-full h-[3px] bg-primary rounded-full absolute left-0 ${showNav? 'rotate-[45deg] top-[14px]' : 'rotate-0 top-0'} duration-[.4s]`}></span>
-                <span className={`w-full h-[3px] bg-primary rounded-full absolute top-1/2 translate-y-[-50%] left-0 ${showNav?'hidden' : 'block'} duration-[.4s]`}></span>
-                <span className={`w-full h-[3px] bg-primary rounded-full absolute left-0 ${showNav? 'rotate-[-45deg] top-[50%]' : 'rotate-0 top-[90%]'} duration-[.4s]`}></span>
+            <button
+              onClick={() => setShowNav(!showNav)}
+              className="w-[30px] h-[30px] relative"
+            >
+              <span
+                className={`w-full h-[3px] bg-primary rounded-full absolute left-0 ${
+                  showNav ? "rotate-[45deg] top-[14px]" : "rotate-0 top-0"
+                } duration-[.4s]`}
+              ></span>
+              <span
+                className={`w-full h-[3px] bg-primary rounded-full absolute top-1/2 translate-y-[-50%] left-0 ${
+                  showNav ? "hidden" : "block"
+                } duration-[.4s]`}
+              ></span>
+              <span
+                className={`w-full h-[3px] bg-primary rounded-full absolute left-0 ${
+                  showNav ? "rotate-[-45deg] top-[50%]" : "rotate-0 top-[90%]"
+                } duration-[.4s]`}
+              ></span>
             </button>
 
-            <div className={`p-[5px] w-[90%] rounded-[20px] bg-primary absolute top-[60px] dark:bg-sky-600 ${showNav? 'right-[20px]' : 'right-[-100%]'} duration-[.4s]`}>
+            <div
+              className={`p-[5px] w-[90%] rounded-[20px] bg-primary absolute top-[60px] dark:bg-sky-600 ${
+                showNav ? "scale-[1] z-10" : "scale-[0] z-10"
+              } duration-[.4s]`}
+            >
+              <div className="navButton flex justify-center text-white font-bold mt-[10px] mb-[20px]">
+                <button className="w-[24px] h-[24px] text-[24px]">
+                  <RiUserLine />
+                </button>
+                <button className="w-[24px] h-[24px] text-[24px] ml-[22px] relative">
+                  <PiShoppingCartSimple />
+                  <span className="w-[20px] h-[20px] rounded-full bg-[#0EA5E9] flex justify-center items-center text-[12px] font-medium font-poppins text-[#fff] absolute top-[-5px] right-[-10px]">
+                    3
+                  </span>
+                </button>
+              </div>
 
-                <ul className="flex flex-col items-center gap-[10px] text-[20px] text-white font-poppins font-semibold">
-                    <li><Link to={'/'}>Home</Link></li>
-                    <li><Link to={'/'}>About</Link></li>
-                </ul>
-
+              <ul className="flex flex-col items-center gap-[10px] text-[20px] text-white font-poppins font-semibold">
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/"}>About</Link>
+                </li>
+              </ul>
             </div>
-
-
           </div>
         </div>
       </nav>
